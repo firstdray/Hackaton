1018478d37dbf301800a1ece632490828ecf8d08
firstdray <firstdray@gmail.com>
2024-10-21 12:58:00 +0400

    update signIn.jsx

--- src/components/signIn/signIn.jsx
+++ src/components/signIn/signIn.jsx
@@ -8,6 +8,28 @@ class SignIn extends React.Component {
                 <div className="up">
                     <h1>Вход</h1>
                     <p>Через социальные сети</p>
+                    <div className="socsety">
+                        <a href="/">VK</a>
+                        <a href="/">FC</a>
+                        <a href="/">TW</a>
+                    </div>
+                </div>
+
+                <p>Или</p>
+
+                <div className="inp">
+                    <input type="text" placeholder="Логин"/>
+                    <input type="password" placeholder="Пароль"/>
+                    <label>
+                        <input type="checkbox"/>
+                        <p>Запомнить меня</p>
+                    </label>
+                </div>
+
+                <div className="down">
+                    <button>Войти</button>
+                    <a href="/">Забылир пароль</a>
+                    <p>Ещё нет аккаунта?<a href="/">Зарегистрируйтесь</a></p>
                 </div>
             </div>
         )
